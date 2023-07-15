1138e009f8f75e87cb4305a8993a2b5419d08013
Chandrakanth Chakali <chandrakanth.chakali@alight.com>
2023-07-15 17:03:41 +0530

    show spinner for genre

--- src/components/GameCardComtainer.tsx
+++ src/components/GameCardComtainer.tsx
@@ -8,7 +8,7 @@ interface Props{
 const GameCardComtainer = ({children}:Props) => {
     return (
          //when rendered it returns a DIV
-        <Box height='300px' borderRadius={10} overflow='hidden'>
+        <Box  borderRadius={10} overflow='hidden'>
             {children}
         </Box>
     )
