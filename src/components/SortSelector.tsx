eb387fa565ebb33b50b7b2f497200abc393991f8
Chandrakanth Chakali <chandrakanth.chakali@alight.com>
2023-07-16 17:09:40 +0530

    Sort Games

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,23 +1,40 @@
-import { Menu, MenuButton, Button, MenuList, MenuItem } from '@chakra-ui/react'
-import React from 'react'
-import { BsChevronDown } from 'react-icons/bs'
+import { Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react";
+import React from "react";
+import { BsChevronDown } from "react-icons/bs";
 
-const SortSelector = () => {
-    return (
-    <Menu>
-      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        Order by rel
-      </MenuButton>
-      <MenuList>
-       <MenuItem>R</MenuItem>
-       <MenuItem>D</MenuItem>
-       <MenuItem>Na</MenuItem>
-       <MenuItem>Rd</MenuItem>
-       <MenuItem>PP</MenuItem>
-       <MenuItem>AA</MenuItem>
-      </MenuList>
-    </Menu>
-    )
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  sortOrder: string;
 }
 
-export default SortSelector
+const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
+  const sortOrders = [
+    { value: "", label: "Relevance" },
+    { value: "-added", label: "Date added" },
+    { value: "name", label: "Name" },
+    { value: "-released", label: "Release date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Average rating" },
+  ];
+  const currentSortOrder = sortOrders.find(order=>order.value===sortOrder);
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        Order by :{currentSortOrder?.label || 'Relevance'}
+      </MenuButton>
+      <MenuList>
+        {sortOrders.map((order) => (
+          <MenuItem
+            onClick={() => onSelectSortOrder(order.value)}
+            key={order.value}
+            value={order.value}
+          >
+            {order.label}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default SortSelector;
