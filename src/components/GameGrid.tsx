4df52d7b5d51d17f3f41e8924bead68df9e0c684
Chandrakanth Chakali <chandrakanth.chakali@alight.com>
2023-07-16 21:26:06 +0530

    ship genre static data

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,8 +1,9 @@
 import { SimpleGrid, Text } from "@chakra-ui/layout";
+import React from "react";
 import { GameQuery } from "../App";
 import useGames, {  Platform } from "../hooks/useGames";
 import { Genre } from "../hooks/useGenres";
-import GameCard from "./Gamecard";
+import GameCard from "./GameCard";
 import GameCardComtainer from "./GameCardComtainer";
 import GameCardSkeleton from "./GameCardSkeleton";
 
